da7373e919bc8dfdbdbd57d48c26be6744ca5e9d
Archifirst <ha2333503@naver.com>
2026-09-18 00:03:00 +0900

    Create MainActivity.java

---
+++ app/src/main/java/com/ccc/colorstyler/MainActivity.java
@@ -0,0 +1,42 @@
+package com.ccc.colorstyler;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+
+        // 배포된 Streamlit 주소 로드
+        webView.loadUrl("https://clothing-color-combination-gkziplqxyso9hghm8mhapy.streamlit.app");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
